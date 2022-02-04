6a4acb825c537fe66c02aa1e69bdd66e85c92273
jtissier <jtdevweb@gmail.com>
2022-02-04 15:56:15 +0100

    Begin Graphique version for application

---
+++ Application/template/head.php
@@ -0,0 +1,26 @@
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="fr-FR" xml:lang="fr-FR">
+
+<head>
+    <!-- Meta -->
+    <meta charset="UTF-8">
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8" /> 
+	<meta http-equiv="content-language" content="fr-FR" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    
+    <!-- Link -->
+
+    <!-- StyleSheet -->
+    <link rel="stylesheet" type="text/css" href="./css/Style.css"/>
+      
+    <!-- Script -->
+    <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
+       
+    <!-- Title -->
+    <title> Gestion Magasin Lycée Henri Brisson?> </title>
+</head>
+
